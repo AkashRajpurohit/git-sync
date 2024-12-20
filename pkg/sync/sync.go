f529232d72603f8b0b32a9a6ced85fec1df65406
AkashRajpurohit <me@akashrajpurohit.com>
2024-12-20 12:48:32 +0530

    fix: :bug: use token manager for sync operations

--- pkg/sync/sync.go
+++ pkg/sync/sync.go
@@ -9,7 +9,14 @@ import (
 
 	"github.com/AkashRajpurohit/git-sync/pkg/config"
 	"github.com/AkashRajpurohit/git-sync/pkg/logger"
+	"github.com/AkashRajpurohit/git-sync/pkg/token"
 )
+
+var tokenManager *token.Manager
+
+func InitTokenManager(tokens []string) {
+	tokenManager = token.NewManager(tokens)
+}
 
 func getBaseDirectoryPath(repoOwner, repoName string, config config.Config) string {
 	return filepath.Join(config.BackupDir, repoOwner, repoName)
@@ -56,8 +63,12 @@ func getGitFetchCommand(CloneType, repoPath, repoURL string) *exec.Cmd {
 }
 
 func CloneOrUpdateRepo(repoOwner, repoName string, config config.Config) {
+	if tokenManager == nil {
+		InitTokenManager(config.Tokens)
+	}
+
 	repoFullName := fmt.Sprintf("%s/%s", repoOwner, repoName)
-	repoURL := fmt.Sprintf("%s://%s:%s@%s/%s.git", config.Server.Protocol, config.Username, config.Token, config.Server.Domain, repoFullName)
+	repoURL := fmt.Sprintf("%s://%s:%s@%s/%s.git", config.Server.Protocol, config.Username, tokenManager.GetNextToken(), config.Server.Domain, repoFullName)
 	repoPath := filepath.Join(getBaseDirectoryPath(repoOwner, repoName, config), repoName+".git")
 
 	if _, err := os.Stat(repoPath); os.IsNotExist(err) {
@@ -114,14 +125,18 @@ func CloneOrUpdateRawRepo(repoOwner, repoName, repoURL string, config config.Con
 }
 
 func SyncWiki(repoOwner, repoName string, config config.Config) {
+	if tokenManager == nil {
+		InitTokenManager(config.Tokens)
+	}
+
 	repoFullName := fmt.Sprintf("%s/%s", repoOwner, repoName)
-	repoWikiURL := fmt.Sprintf("%s://%s:%s@%s/%s.wiki.git", config.Server.Protocol, config.Username, config.Token, config.Server.Domain, repoFullName)
+	repoWikiURL := fmt.Sprintf("%s://%s:%s@%s/%s.wiki.git", config.Server.Protocol, config.Username, tokenManager.GetNextToken(), config.Server.Domain, repoFullName)
 	repoWikiPath := filepath.Join(getBaseDirectoryPath(repoOwner, repoName, config), repoName+".wiki.git")
 
 	// Special handling for bitbucket since it does not follow the traditional pattern for wiki repos
 	// @see here: https://support.atlassian.com/bitbucket-cloud/docs/clone-a-wiki/
 	if config.Platform == "bitbucket" {
-		repoWikiURL = fmt.Sprintf("%s://%s:%s@%s/%s.git/wiki", config.Server.Protocol, config.Username, config.Token, config.Server.Domain, repoFullName)
+		repoWikiURL = fmt.Sprintf("%s://%s:%s@%s/%s.git/wiki", config.Server.Protocol, config.Username, tokenManager.GetNextToken(), config.Server.Domain, repoFullName)
 	}
 
 	if _, err := os.Stat(repoWikiPath); os.IsNotExist(err) {
